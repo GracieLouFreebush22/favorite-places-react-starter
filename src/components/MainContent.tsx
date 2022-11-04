6c23510a9cd27559c4bb738651966f5244814134
grace k <grace.kuperman5472@gmail.com>
2022-11-04 13:23:11 +0000

    added div

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -2,7 +2,14 @@ import PlaceItem from "./components/PlaceItem"
 
 function MainContent() :JSX.Element {
     return (
-       <p> hello main content</p>
+    <div>
+        <p> hello main content</p>
+       <main>
+            <PlaceItem/>
+       </main>
+    </div>
+       
+
     )
 }
 
