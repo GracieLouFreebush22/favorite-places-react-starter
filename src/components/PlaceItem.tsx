2e6ccd3acffd8fec78963e0067ac34625d6f62e1
grace k <grace.kuperman5472@gmail.com>
2022-11-04 13:51:33 +0000

    props added

--- src/components/PlaceItem.tsx
+++ src/components/PlaceItem.tsx
@@ -1,19 +1,24 @@
-interface PlaceItemProps{
-    title: string;
-    name: string;
-    country: string;
-    image: string;
-    maps: string;
-    explain: string;
-
+interface PlaceItemProps {
+  title: string;
+  name: string;
+  country: string;
+  image: string;
+  maps: string;
+  explain: string;
 }
 
 function PlaceItem(props: PlaceItemProps): JSX.Element {
-    return (
-        <p> hello place item</p>
-    )
+  return (
+    <section>
+        <h2> {props.title}</h2>
+        <h3> {props.name} | {props.country}</h3>
+        <img>{props.image} </img> 
+        <p> {props.explain} </p>
+        <p> {props.maps}</p>
+    </section>
+  )
+  
+  
 }
-
-
 
 export default PlaceItem;
